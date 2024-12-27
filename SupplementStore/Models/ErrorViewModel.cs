3f80fbb452b16f872f8f3ce46fe3cb90e2333332
moham <moham@MOHAMEDPC>
2024-12-27 17:30:47 +0200

    Add project files.

---
+++ SupplementStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SupplementStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
